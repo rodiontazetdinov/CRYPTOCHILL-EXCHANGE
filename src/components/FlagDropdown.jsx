9c2c3071909d873b161588f0aed2d374a4e2a3ae
Якимов Артем Дмитриевич <yakimov_a_qschool@qsoft.ru>
2023-12-29 21:02:59 +0300

    Добавлено состояние языка

--- src/components/FlagDropdown.jsx
+++ src/components/FlagDropdown.jsx
@@ -1,24 +1,34 @@
 import { Link } from "react-router-dom";
 
 import russia from "../images/rf-flag.svg";
-import england from "../images/uk-flag.svg";
+import england from "../images/gb-flag.svg";
 import ukraine from "../images/ua-flag.svg";
 import georgia from "../images/ge-flag.svg";
 import poland from "../images/pl-flag.svg";
+import { useDispatch } from "react-redux";
+import { setLanguage } from "../store/actions";
 
 export const FlagDropdown = () => {
-
     const flagMenuData = [
-      {name: 'Русский', to: "/", icon: russia},
-      {name: 'English', to: "/", icon: england},
-      {name: 'Украïнська', to: "/", icon: ukraine},
-      {name: 'Georgian', to: "/", icon: georgia},
-      {name: 'Polish', to: "/", icon: poland},
-    ]
+      {name: 'Русский', code: 'ru', to: "/", icon: russia},
+      {name: 'English', code: 'en', to: "/", icon: england},
+      {name: 'Украïнська', code: 'ua', to: "/", icon: ukraine},
+      {name: 'Georgian', code: 'ge', to: "/", icon: georgia},
+      {name: 'Polish', code: 'pl', to: "/", icon: poland},
+    ];
+
+    const dispatch = useDispatch();
+
     return (
         <ul className="flex flex-col absolute min-w-[240px] rounded-2xl bg-drop backdrop-blur text-base left-0 top-12 w-max py-1 z-10">
             {flagMenuData.map((menuItem) => { return (
-              <li className="flex flex-row items-center whitespace-nowrap cursor-pointer">
+              <li
+                className="flex flex-row items-center whitespace-nowrap cursor-pointer"
+                onClick={() => {
+                  console.log(menuItem.code);
+                  dispatch(setLanguage(menuItem.code))
+                }}
+              >
                 <Link to={menuItem.to} className="flex w-full items-center whitespace-nowrap px-4 py-1">
                   <img className="mr-1 w-11 rounded-lg" src={menuItem.icon} alt="иконка персональных данных" />
                   {menuItem.name}
--- src/components/LanguageMenu.jsx
+++ src/components/LanguageMenu.jsx
@@ -1,4 +1,9 @@
-import flag from "../images/rf-flag.svg";
+import flagGb from "../images/gb-flag.svg";
+import flagGe from "../images/ge-flag.svg";
+import flagPl from "../images/pl-flag.svg";
+import flagRu from "../images/rf-flag.svg";
+import flagUa from "../images/ua-flag.svg";
+
 import arrowUp from "../images/arrow-up.svg";
 import arrowDown from "../images/arrow-down.svg";
 import { Navbar } from "./Navbar";
@@ -10,7 +15,16 @@ import { closeDropdown, openDropdown } from "../store/actions";
 export const LanguageMenu = () => {
 
   const dropdownOpen = useSelector((state) => state.dropdowns.flag);
+  const language = useSelector((state) => state.language);
   const dispatch = useDispatch();
+
+  const flags = {
+    "ua": flagUa,
+    "ge": flagGe,
+    "pl": flagPl,
+    "ru": flagRu,
+    "en": flagGb
+  }
 
   return (
     <div
@@ -20,7 +34,7 @@ export const LanguageMenu = () => {
         ev.stopPropagation();
       }}
     >
-      <img src={flag} alt="flag " className="rounded-lg" />
+      <img src={flags[language]} alt="flag " className="rounded-lg" />
       <img src={dropdownOpen ? arrowUp : arrowDown} alt="arrow" className="ml-2"/>
       {dropdownOpen && (
           <FlagDropdown />
